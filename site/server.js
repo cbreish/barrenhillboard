9d01fa61fa7e73b931c3ff5874a05698328ff11d
cbreish <cbreish@gmail.com>
2016-01-10 16:58:35 -0500

    Setup KOA host.  Add static dashboard sample.

--- site/server.js
+++ site/server.js
@@ -1,7 +1,21 @@
-var http = require('http');
-var port = process.env.port || 1338;
-http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World\n');
-}).listen(port);
+var router = require('koa-router')();
+var serve = require('koa-static');
+var serveFolder = require('koa-static-folder');
+var logger = require('koa-logger');
+var koaBody = require('koa-body');
+var koa = require('koa.io');
+var app = koa();
+//Setup koa routes
+app.use(serve('./public'));
+app.use(serveFolder('./css'));
+app.use(serveFolder('./js'));
+app.use(serveFolder('./img'));
+//Setup koa app
+app.use(logger());
+app.use(koaBody());
+app.use(router.routes());
+app.outputErrors = true;
+var port = 1338;
+app.listen(port);
+console.log('listening on ' + port);
 //# sourceMappingURL=server.js.map
