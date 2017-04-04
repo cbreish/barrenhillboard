2853848b116b9e92175e4fddd7f484c010a875e6
cbreish <cbreish@gmail.com>
2017-04-04 18:15:27 -0400

    Cleaned up old files, added auto refresh, changed image counts

--- site/widgets/images.js
+++ site/widgets/images.js
@@ -1,12 +1,12 @@
 "use strict";
 var every = require('schedule').every;
-var Images = (function () {
+var Images = (function() {
     function Images(bus) {
         var _this = this;
-        this.rotateImage = function () {
+        this.rotateImage = function() {
             _this.currentImage = 'http://barrenhillboard.com/images/' + _this.getNextImage();
         };
-        this.updateWidgets = function () {
+        this.updateWidgets = function() {
             console.log('Updating image');
             _this.bus.post({
                 event: 'widgetUpdate',
@@ -14,7 +14,7 @@ var Images = (function () {
                 messageData: { image: _this.currentImage }
             });
         };
-        this.getNextImage = function () {
+        this.getNextImage = function() {
             _this.specialShowing = !_this.specialShowing;
             if (_this.specialShowing) {
                 return 'image_special_' + _this.pad(_this.randomNumber(_this.maxSpecialImage), 3) + '.jpg';
@@ -22,22 +22,22 @@ var Images = (function () {
                 return 'image' + _this.pad(_this.randomNumber(_this.maxImage), 3) + '.jpg';
             }
         };
-        this.randomNumber = function (max) {
+        this.randomNumber = function(max) {
             return Math.floor(Math.random() * max) + 1;
         };
         this.specialShowing = false;
-        this.maxImage = 83;
-        this.maxSpecialImage = 1;
+        this.maxImage = 57;
+        this.maxSpecialImage = 5;
         var instance = this;
         instance.rotateImage();
         bus.subscribe({ event: 'userConnected' }, instance.updateWidgets);
-        every('30s').do(function () {
+        every('30s').do(function() {
             instance.rotateImage();
             instance.updateWidgets();
         });
         this.bus = bus;
     }
-    Images.prototype.pad = function (n, width) {
+    Images.prototype.pad = function(n, width) {
         n = n + '';
         return n.length >= width ? n : new Array(width - n.length + 1).join('0') + n;
     };
